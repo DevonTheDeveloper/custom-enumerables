0837fb3cbb82fad61ece33e8eab24c2f9dad5ce7
Devon <contactdevon@proton.me>
2023-04-24 23:49:24 +0200

    Add #my_each enumerable

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -1,1 +1,22 @@
+# frozen_string_literal: true
 
+module Enumerable
+  # Your code goes here
+end
+
+# You will first have to define my_each
+# on the Array class. Methods defined in
+# your enumerable module will have access
+# to this method
+class Array
+  # Define my_each here
+  def my_each
+    index = 0
+
+    length.times do
+      yield(self[index])
+      index += 1
+    end
+    self
+  end
+end
